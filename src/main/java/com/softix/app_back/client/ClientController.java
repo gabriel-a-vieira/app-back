c3a7cc524541331cb2c69b0e43fc622be7bb1965
Gabriel Vieira <vieira.gabriel7901@gmail.com>
2025-09-09 14:57:24 -0300

    Implemented JpaRepository in ClientRepository, and start to use this class on controller, for some finds

--- src/main/java/com/softix/app_back/client/ClientController.java
+++ src/main/java/com/softix/app_back/client/ClientController.java
@@ -3,6 +3,10 @@ package com.softix.app_back.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("client")
 public class ClientController {
@@ -10,14 +14,17 @@ public class ClientController {
     @Autowired
     ClientService clientService;
 
+    @Autowired
+    ClientRepository clientRepository;
+
     @GetMapping
-    public String getAll() {
-        return "GET";
+    public List<Client> getAll() {
+        return clientRepository.findAll();
     }
 
     @GetMapping(path = "{id}")
-    public String getById(@PathVariable("id") String id) {
-        return id;
+    public Optional<Client> getById(@PathVariable("id") UUID id) {
+        return clientRepository.findById(id);
     }
 
     @PostMapping
--- src/main/java/com/softix/app_back/client/ClientRepository.java
+++ src/main/java/com/softix/app_back/client/ClientRepository.java
@@ -1,4 +1,9 @@
 package com.softix.app_back.client;
 
-public class ClientRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ClientRepository extends JpaRepository<Client, UUID> {
+
 }
