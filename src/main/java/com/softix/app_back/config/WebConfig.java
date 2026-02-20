15b296eda11be10ae33ac1fd2e990fef23723c17
Gabriel Vieira <vieira.gabriel7901@gmail.com>
2026-02-19 23:24:04 -0300

    Adding CORS in the backend to allow flutter make requests

---
+++ src/main/java/com/softix/app_back/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.softix.app_back.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:45587")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
