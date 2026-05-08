a073f868b10756e97dc09b9c637679894a3b023f
Gabriel Alexandre Vieira <vieira.gabriel7901@gmail.com>
2026-05-08 18:16:17 -0300

    Adding more user information in the LoginResponse

--- src/main/java/com/softix/app_back/auth/AuthController.java
+++ src/main/java/com/softix/app_back/auth/AuthController.java
@@ -46,7 +46,7 @@ public class AuthController {
         User user = (User) authentication.getPrincipal();
         String token = tokenConfig.generateToken(user);
 
-        return ResponseEntity.ok(new LoginResponse(token));
+        return ResponseEntity.ok(new LoginResponse(token, user.getName(), user.getEmail()));
 
     }
 
--- src/main/java/com/softix/app_back/auth/response/LoginResponse.java
+++ src/main/java/com/softix/app_back/auth/response/LoginResponse.java
@@ -1,4 +1,6 @@
 package com.softix.app_back.auth.response;
 
-public record LoginResponse(String token) {
+public record LoginResponse(String token,
+                            String name,
+                            String email) {
 }
